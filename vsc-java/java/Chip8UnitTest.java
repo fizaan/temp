aa3de8db139c0282fed09cb627b0b2aeb1aa2897
Faiz <fizaan@gmail.com>
2021-02-14 11:19:00 -0500

    More opcode conditions

--- vsc-java/java/Chip8UnitTest.java
+++ vsc-java/java/Chip8UnitTest.java
@@ -9,10 +9,11 @@ import java.util.ArrayList;
 
 public class Chip8UnitTest {
     public static void main(String args[]) throws Exception {
-        String rom = "C:/Users/Alifa/Desktop/lc3-obj/chip8roms/test1.BC_test.bin";
+        String rom = "C:/Users/Alifa/Desktop/lc3-backup/eclipse/LC3Chip8Emulator/lc3-obj/chip8roms/test1.bin";
         InputStream input = new FileInputStream(rom);  
         DataInputStream inst = new DataInputStream(input);
         int index = 0;
+        int counter = 0;
         ArrayList<String> data = data=new ArrayList<String>();
         while((inst.available())>0) { 
             int instruction = inst.readChar();
@@ -21,21 +22,59 @@ public class Chip8UnitTest {
             int r2 = (instruction >> 4) & 0xf;
             int lowbyte = instruction & 0xff;
             int last4bits = instruction & 0xf;
+            String toAppend = null;
             if(args[0].equals("hex"))
-                data.add(String.format("%02X %02X %02X %02X %02X %02X",instruction,opcode,r1,r2,lowbyte,last4bits));
+                toAppend = String.format("%02X - %02X: %02X %02X %02X %02X %02X",counter,instruction,opcode,r1,r2,lowbyte,last4bits);
             else
-                data.add(String.format("%d %d %d %d %d %d",instruction,opcode,r1,r2,lowbyte,last4bits));
+                toAppend = String.format("%d - %d: %d %d %d %d %d",counter,instruction,opcode,r1,r2,lowbyte,last4bits);
+            
+            counter++;
+
+            if(opcode == 0)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 1)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 2)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 3)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 4)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 5)
+                toAppend += String.format(" | opcode = %02X",opcode);    
+            if(opcode == 6)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 7)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 8)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 9)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 0xa)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 0xb)
+                toAppend += String.format(" | opcode = %02X",opcode); 
+            if(opcode == 0xc)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 0xd)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 0xe)
+                toAppend += String.format(" | opcode = %02X",opcode);
+            if(opcode == 0xf)
+                toAppend += String.format(" | opcode = %02X",opcode);     
+            
+            data.add(toAppend);
         }
 
         /*
             dos>cd C:\Users\Alifa\Desktop\lc3-backup\eclipse\LC3Compiler\ant
             dos>2.compile.bat src\chip8\chip8emu.lct
-            dos>3.run.bat out\chip8emu.lct_asm.txt out\chip8emu.lct_asm.obj hex > C:\Users\Alifa\Desktop\lc3-obj\chip8out\out.txt
+            dos>3.run.bat out\chip8emu.lct_asm.txt out\chip8emu.lct_asm.obj hex > C:\Users\Alifa\Desktop\lc3-backup\eclipse\LC3Chip8Emulator\lc3-obj\chip8out\out.txt
             dos>cd C:\Users\Alifa\Desktop\mystuff\faiz.dev.root\gitdir\github-fizaan\temp\vsc-java\java
             dos>java Chip8UnitTest.java hex
         */
 
-        String lc3chip8out = "C:/Users/Alifa/Desktop/lc3-obj/chip8out/out.txt";
+        String lc3chip8out = "C:/Users/Alifa/Desktop/lc3-backup/eclipse/LC3Chip8Emulator/lc3-obj/chip8out/out.txt";
         BufferedReader reader=new BufferedReader(new InputStreamReader(new FileInputStream(lc3chip8out)));
         String line;
         while((line=reader.readLine())!=null)
