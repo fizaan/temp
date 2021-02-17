13fa910e33b491f979cd209acebffb9523d492cd
Faiz <fizaan@gmail.com>
2021-02-17 15:10:12 -0500

    Replaced if/else with switch case

--- vsc-java/java/Chip8UnitTest.java
+++ vsc-java/java/Chip8UnitTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class Chip8UnitTest {
     public static void main(String args[]) throws Exception {
-        String rom = "C:/Users/Alifa/Desktop/lc3-backup/eclipse/LC3Chip8Emulator/lc3-obj/chip8roms/test1.bin";
+        String rom = "C:/Users/Alifa/Desktop/lc3-backup/eclipse/LC3Chip8Emulator/lc3-obj/chip8roms/test2.bin";
         InputStream input = new FileInputStream(rom);  
         DataInputStream inst = new DataInputStream(input);
         int index = 0;
@@ -22,7 +22,7 @@ public class Chip8UnitTest {
             int r2 = (instruction >> 4) & 0xf;
             int lowbyte = instruction & 0xff;
             int last4bits = instruction & 0xf;
-            String toAppend = null;
+            String toAppend = "";
             if(args[0].equals("hex"))
                 toAppend = String.format("%02X - %02X: %02X %02X %02X %02X %02X",counter,instruction,opcode,r1,r2,lowbyte,last4bits);
             else
@@ -30,46 +30,146 @@ public class Chip8UnitTest {
             
             counter++;
 
-            if(opcode == 0)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 1)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 2)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 3)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 4)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 5)
-                toAppend += String.format(" | opcode = %02X",opcode);    
-            if(opcode == 6)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 7)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 8)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 9)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 0xa)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 0xb)
-                toAppend += String.format(" | opcode = %02X",opcode); 
-            if(opcode == 0xc)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 0xd)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 0xe)
-                toAppend += String.format(" | opcode = %02X",opcode);
-            if(opcode == 0xf)
-                toAppend += String.format(" | opcode = %02X",opcode);     
-            
+            switch(opcode) {
+                case 0: {
+                    switch(lowbyte) {
+                        case 0xe0:
+                            toAppend += String.format(" | opcode = %02X %02X CLS",opcode,lowbyte);
+                        break;
+                        case 0xee:
+                            toAppend += String.format(" | opcode = %02X %02X RET",opcode,lowbyte);
+                        break;
+                        default:
+                            toAppend += String.format(" | opcode = %02X %02X IGNORE",opcode,lowbyte);
+                        break;
+                    }
+                }
+                break;
+
+                case 1:
+                    toAppend += String.format(" | opcode = %02X COMPUTE3",opcode);
+                break;
+
+                case 2:
+                    toAppend += String.format(" | opcode = %02X COMPUTE4",opcode);
+                break;
+
+                case 3:
+                    toAppend += String.format(" | opcode = %02X COMPUTE5",opcode);
+                break;
+
+                case 4:
+                    toAppend += String.format(" | opcode = %02X COMPUTE6",opcode);
+                break;
+
+                case 5:
+                    toAppend += String.format(" | opcode = %02X COMPUTE7",opcode); 
+                break;
+
+                case 6:
+                    toAppend += String.format(" | opcode = %02X COMPUTE8",opcode);
+                break;
+
+                case 7:
+                    toAppend += String.format(" | opcode = %02X COMPUTE9",opcode);
+                break;
+
+                case 8:
+                    toAppend += String.format(" | opcode = %02X",opcode);
+                break;
+
+                case 9:
+                    toAppend += String.format(" | opcode = %02X COMPUTE10",opcode);
+                break;
+
+                case 0xa:
+                    toAppend += String.format(" | opcode = %02X COMPUTE11",opcode);
+                break;
+
+                case 0xb:
+                    toAppend += String.format(" | opcode = %02X COMPUTE12",opcode);
+                break;
+
+                case 0xc:
+                    toAppend += String.format(" | opcode = %02X COMPUTE13",opcode);
+                break;
+
+                case 0xd:
+                    toAppend += String.format(" | opcode = %02X COMPUTE14",opcode);
+                break;
+
+                case 0xe: {
+                    switch (lowbyte) {
+                        case 0x9e:
+                            toAppend += String.format(" | opcode = %02X %02X UNIMPLEMENTED CODE",opcode,lowbyte);
+                        break;
+                        case 0xa1:
+                            toAppend += String.format(" | opcode = %02X %02X UNIMPLEMENTED CODE",opcode,lowbyte);
+                        break;
+                        default:
+                            toAppend += String.format(" | opcode = %02X %02X Err NO OPCPDE",opcode,lowbyte);
+                        break;
+                    }
+                }
+                break;
+
+                case 0xf: {
+                    switch (lowbyte) {
+                        case 7:
+                            toAppend += String.format(" | opcode = %02X %02X LD[ %02X ],",opcode,lowbyte,r1);
+                        break;
+                        
+                        case 0xa:
+                            toAppend += String.format(" | opcode = %02X %02X UNIMPLEMENTED CODE",opcode,lowbyte);
+                        break;
+                        
+                        case 0x15:
+                            toAppend += String.format(" | opcode = %02X %02X LD delaytimer reg[ %02X ]",opcode,lowbyte,r1); 
+                        break;
+                        
+                        case 0x08:
+                            toAppend += String.format(" | opcode = %02X %02X LD soundtimer reg[ %02X ]",opcode,lowbyte,r1); 
+                        break;
+                        
+                        case 0x1e:
+                            toAppend += String.format(" | opcode = %02X %02X COMPUTE15",opcode,lowbyte);
+                        break;
+                        
+                        case 0x29:
+                            toAppend += String.format(" | opcode = %02X %02X COMPUTE16",opcode,lowbyte);
+                        break;
+
+                        case 0x33:
+                            toAppend += String.format(" | opcode = %02X %02X COMPUTE17",opcode,lowbyte);
+                        break;
+
+                        case 0x55:
+                            toAppend += String.format(" | opcode = %02X %02X COMPUTE18",opcode,lowbyte);
+                        break;
+
+                        case 0x65:
+                            toAppend += String.format(" | opcode = %02X %02X COMPUTE19",opcode,lowbyte);
+                        break;
+                    
+                        default:
+                            toAppend += String.format(" | opcode = %02X %02X Err NO OPCPDE",opcode,lowbyte);
+                        break;
+                    }
+                }
+                break;
+
+                default:
+                    toAppend += "Error: No opcode found"; 
+                break;
+            }
+
             data.add(toAppend);
         }
 
         /*
-            dos>cd C:\Users\Alifa\Desktop\lc3-backup\eclipse\LC3Compiler\ant
+            dos>cd C:\Users\Alifa\Desktop\lc3-backup\eclipse\LC3Chip8Emulator\ant
             dos>2.compile.bat src\chip8\chip8emu.lct
-            dos>3.run.bat out\chip8emu.lct_asm.txt out\chip8emu.lct_asm.obj hex > C:\Users\Alifa\Desktop\lc3-backup\eclipse\LC3Chip8Emulator\lc3-obj\chip8out\out.txt
+            dos>3.run.bat out\chip8emu.lct_asm.txt out\chip8emu.lct_asm.obj hex > ..\lc3-obj\chip8out\out.txt
             dos>cd C:\Users\Alifa\Desktop\mystuff\faiz.dev.root\gitdir\github-fizaan\temp\vsc-java\java
             dos>java Chip8UnitTest.java hex
         */
